054b9454b198584a82bc67a9f8f6fd455c38427a
Mnemovate <mnemovate@gmail.com>
2025-03-20 14:31:39 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:radior/bloc/radio_bloc.dart';
-import 'package:radior/bloc/radio_event.dart';
 import 'package:radior/data/radio_stations.dart';
-import 'package:radior/views/radio_player_screen.dart';
+import 'package:radior/theme/color.dart';
+import 'package:radior/views/onboarding.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Radior.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(backgroundColor: Colors.white),
-        scaffoldBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: Radior.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: BlocProvider(
-        create: (context) => RadioBloc(stations)..add(PlayPauseEvent()),
-        child: const RadioPlayerScreen(),
+        create: (context) => RadioBloc(stations),
+        child: const Onboarding(),
       ),
     ),
   );
