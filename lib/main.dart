85c31ef3ee0b3e95aa126e3249f1659c90d0b4e7
Mnemovate <mnemovate@gmail.com>
2025-03-20 14:56:27 +0700

    consistency theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,59 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:radior/bloc/radio_bloc.dart';
 import 'package:radior/data/radio_stations.dart';
+import 'package:radior/screens/onboarding_screen.dart';
 import 'package:radior/theme/color.dart';
-import 'package:radior/views/onboarding.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    systemNavigationBarColor: Radior.white,
-    systemNavigationBarIconBrightness: Brightness.dark,
-  ));
-  runApp(
-    MaterialApp(
+  runApp(Radior());
+}
+
+class Radior extends StatefulWidget {
+  const Radior({super.key});
+
+  @override
+  State<Radior> createState() => _RadiorState();
+}
+
+class _RadiorState extends State<Radior> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _setSystemUIOverlay();
+  }
+
+  void _setSystemUIOverlay() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: RadiorColor.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    super.didChangePlatformBrightness();
+    _setSystemUIOverlay();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Radior.white,
+        scaffoldBackgroundColor: RadiorColor.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: BlocProvider(
         create: (context) => RadioBloc(stations),
-        child: const Onboarding(),
+        child: const OnboardingScreen(),
       ),
-    ),
-  );
+    );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 }
