1b8338a1ca26fb804f2de35b7165dfaab257f521
Mnemovate <mnemovate@gmail.com>
2025-03-22 00:58:30 +0700

    add color

---
+++ lib/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RadiorColor {
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff2F2E36);
+  static const Color black80 = Color(0xff59585E);
+  static const Color green = Color(0xff4CAF50);
+}
