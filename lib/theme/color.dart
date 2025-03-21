92c19d44fa7056cf073fe0bfa03fe5d805ebbf07
Mnemovate <mnemovate@gmail.com>
2025-03-22 00:58:21 +0700

    rename

--- lib/theme/color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class RadiorColor {
-  static const Color white = Color(0xffFFFFFF);
-  static const Color black = Color(0xff2F2E36);
-  static const Color black80 = Color(0xff59585E);
-}
