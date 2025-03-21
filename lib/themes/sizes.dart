b2b957741a311ac4504b0968f6732331c6d30e97
Mnemovate <mnemovate@gmail.com>
2025-03-22 00:58:40 +0700

    add sizes

---
+++ lib/themes/sizes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+SizedBox get spaceHeigh20 => SizedBox(height: 20);
+
+SizedBox get spaceHeight50 => SizedBox(height: 50);
+
+SizedBox get spaceHeight60 => SizedBox(height: 60);
