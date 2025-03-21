4441cb8c13320ea397f7c6510a95485b4a076d8b
Mnemovate <mnemovate@gmail.com>
2025-03-22 00:58:48 +0700

    add text

---
+++ lib/themes/texts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:radior/themes/colors.dart';
+
+TextStyle get titleOnboarding => TextStyle(
+  fontSize: 26, 
+  color: RadiorColor.black,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'SFProText',
+);
+
+TextStyle get descriptionOnboarding => TextStyle(
+  fontSize: 15, 
+  color: RadiorColor.black80,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'SFProText',
+);
