027bda584ed039f524f46259429c576b489d50e4
Mnemovate <mnemovate@gmail.com>
2025-03-24 17:07:12 +0700

    remove station unwork

--- lib/data/radio_stations.dart
+++ lib/data/radio_stations.dart
@@ -5,59 +5,59 @@ import 'package:radior/models/radio_station.dart';
 
 final List<RadioStation> stations = [
   RadioStation(
-    name: 'Jakarta - 92.0 fm',
+    name: 'Sonora Jakarta - 92.0 fm',
     streamUrl: 'https://cast1.my-control-panel.com/proxy/radioso1/stream',
     imageUrl: 'https://www.sonora.co.id//assets/v2/images/network/jakarta.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Surabaya - 98.0 fm',
+    name: 'Sonora Surabaya - 98.0 fm',
     streamUrl: 'https://cast3.asurahosting.com/proxy/radios25/stream',
     imageUrl: 'https://www.sonora.co.id//assets/v2/images/network/surabaya.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Palembang - 102.6 fm',
+    name: 'Sonora Palembang - 102.6 fm',
     streamUrl: 'https://cast2.my-control-panel.com/proxy/radios21/stream',
     imageUrl:
         'https://www.sonora.co.id//assets/v2/images/network/palembang.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Yogyakarta - 97.4 fm',
+    name: 'Sonora Yogyakarta - 97.4 fm',
     streamUrl: 'https://cast3.asurahosting.com/proxy/radios28/stream',
     imageUrl:
         'https://www.sonora.co.id//assets/v2/images/network/yogyakarta.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Semarang - 98.9 fm',
+    name: 'Sonora Semarang - 98.9 fm',
     streamUrl: 'https://cast3.asurahosting.com/proxy/radios30/stream',
     imageUrl: 'https://www.sonora.co.id//assets/v2/images/network/semarang.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Pangkalpinang - 101.1 fm',
+    name: 'Sonora Pangkalpinang - 101.1 fm',
     streamUrl: 'https://cast2.my-control-panel.com/proxy/radios18/stream',
     imageUrl:
         'https://www.sonora.co.id//assets/v2/images/network/pangkalpinang.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Pontianak - 96.7 fm',
+    name: 'Sonora Pontianak - 96.7 fm',
     streamUrl: 'https://cast3.asurahosting.com/proxy/radios29/stream',
     imageUrl:
         'https://www.sonora.co.id//assets/v2/images/network/pontianak.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Bali - 98.9 fm',
+    name: 'Sonora Bali - 98.9 fm',
     streamUrl: 'https://cast1.my-control-panel.com/proxy/radios12/stream',
     imageUrl: 'https://www.sonora.co.id//assets/v2/images/network/bali.png',
     description: '...',
   ),
   RadioStation(
-    name: 'Solo - 98.8 fm',
+    name: 'Sonora Solo - 98.8 fm',
     streamUrl: 'https://cast1.my-control-panel.com/proxy/radioso6/stream',
     imageUrl: 'https://www.sonora.co.id//assets/v2/images/network/solo.png',
     description: '...',
@@ -76,64 +76,16 @@ final List<RadioStation> stations = [
     description: 'news, culture, education, entertainment',
   ),
   RadioStation(
-    name: 'Delta FM',
-    streamUrl: 'https://s1.cloudmu.id/listen/delta_fm/stream',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/8/58058.v29.png',
-    description: 'pop, talk, easy listening',
-  ),
-  RadioStation(
-    name: 'I Radio',
-    streamUrl: 'https://stream.radiojar.com/4ywdgup3bnzuv',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/9/18959.v10.png',
-    description: 'rock, pop, jazz, hip-hop',
-  ),
-  RadioStation(
-    name: 'FeMale Radio',
-    streamUrl: 'https://s1.cloudmu.id/listen/female_radio/stream',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/6/60996.v16.png',
-    description: 'adult contemporary',
-  ),
-  RadioStation(
     name: 'Slow Radio',
     streamUrl: 'https://stream.zeno.fm/dpk2zq5np2zuv',
     imageUrl: 'https://cdn.onlineradiobox.com/img/l/6/91316.v15.png',
     description: "r'n'b, pop, jazz, folk, easy listening, relax, soul, chill-out, 90s, 00s, soft rock, smooth jazz, adult contemporary, acoustic, romantic, love songs, hits, balada",
   ),
   RadioStation(
-    name: 'Bahana FM',
-    streamUrl: 'https://s1.cloudmu.id/listen/bahana_fm/stream',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/8/19708.v25.png',
-    description: 'pop',
-  ),
-  RadioStation(
     name: 'Golden Memories',
     streamUrl: 'https://stream.zeno.fm/xg7mcgf1bf9uv',
     imageUrl: 'https://cdn.onlineradiobox.com/img/l/0/110170.v4.png',
     description: "rock, pop, easy listening, oldies, soft rock, hits"
-  ),
-  RadioStation(
-    name: 'Trax FM',
-    streamUrl: 'https://stream.radiojar.com/rrqf78p3bnzuv',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/6/18806.v12.png',
-    description: 'pop, top40, adult contemporary',
-  ),
-  RadioStation(
-    name: 'Hard Rock FM',
-    streamUrl: 'https://stream.radiojar.com/7csmg90fuqruv',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/0/18880.v14.png',
-    description: 'pop, techno, jazz, alternative',
-  ),
-  RadioStation(
-    name: 'Gen FM',
-    streamUrl: 'https://ic.mari.co.id:8443/genfm',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/6/19026.v33.png',
-    description: 'hits',
-  ),
-  RadioStation(
-    name: 'Jak 101 FM',
-    streamUrl: 'https://ic.mari.co.id:8443/jakfm',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/5/19705.v43.png',
-    description: "dance, rock, r'n'b, pop, easy listening, alternative, adult contemporary, hits",
   ),
   RadioStation(
     name: 'Rock Rewind',
@@ -176,11 +128,5 @@ final List<RadioStation> stations = [
     streamUrl: 'https://cast1.my-control-panel.com/proxy/radioso1/live',
     imageUrl: 'https://cdn.onlineradiobox.com/img/l/6/19176.v14.png',
     description: "pop, hits",
-  ),
-  RadioStation(
-    name: 'Cosmopolitan FM',
-    streamUrl: 'https://stream.radiojar.com/u7d8heq3bnzuv',
-    imageUrl: 'https://cdn.onlineradiobox.com/img/l/8/19028.v13.png',
-    description: "pop, talk, adult contemporary, hits",
   ),
 ];
