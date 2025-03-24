962bfd8ad4f25dd4a0250295637926ac3cb0eed0
Mnemovate <mnemovate@gmail.com>
2025-03-24 15:49:29 +0700

    new sizes

--- lib/themes/sizes.dart
+++ lib/themes/sizes.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-SizedBox get spaceHeigh20 => SizedBox(height: 20);
+SizedBox get spaceHeight20 => SizedBox(height: 20);
+
+SizedBox get spaceHeight40 => SizedBox(height: 40);
 
 SizedBox get spaceHeight50 => SizedBox(height: 50);
 
 SizedBox get spaceHeight60 => SizedBox(height: 60);
+
+SizedBox get spaceWidth20 => SizedBox(width: 20);
+
+SizedBox get spaceWidth50 => SizedBox(width: 50);
+
+SizedBox get spaceWidth60 => SizedBox(width: 60);
